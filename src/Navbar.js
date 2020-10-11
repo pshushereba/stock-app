a0b3df54bdd7027440fd355f1f78c2fdccad4be3
Patrick Shushereba <pshushereba@gmail.com>
2020-10-10 19:23:55 -0700

    Added basic mui navbar.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const Navbar = (props) => {
-    return (
-        <div>
-            <h1>Stock Info</h1>
-        </div>
-    )
-}
+  return (
+    <AppBar>
+      <Toolbar>
+        <h2>Stock Info</h2>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
